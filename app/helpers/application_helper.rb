f6a4c255ed1c107e6b3b1c928f910961da9d0ff9
Plum0329 <seiya19990329@gmail.com>
2025-01-17 00:15:21 +0900

    戻るボタンの修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def back_button(fallback_path = nil)
+    fallback_path ||= root_path
+    render 'shared/back_button', fallback_path: fallback_path
+  end
+
+  def with_turbo_frame_reload(&block)
+    turbo_frame_tag "_top" do
+      capture(&block)
+    end
+  end
 end
