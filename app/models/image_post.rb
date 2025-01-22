3a2bf9bd4c9eec4b8c185a33d860e146f221d4a9
Plum0329 <seiya19990329@gmail.com>
2025-01-22 23:06:16 +0900

    モデル復元

--- app/models/image_post.rb
+++ app/models/image_post.rb
@@ -1,7 +1,31 @@
 class ImagePost < ApplicationRecord
   has_many :posts
-  has_many :themes, dependent: :destroy
+  has_one :theme
+  has_many :posts, dependent: :nullify
   has_one_attached :image
 
-  validates :description, presence: true
+  validates :description, length: { maximum: 10 }
+
+  validates :image,
+    content_type: {
+      in: ['image/png', 'image/jpg', 'image/jpeg'],
+      message: 'はPNG、JPEG、JPG形式のみが対応しています'
+    },
+    size: {
+      less_than: 5.megabytes,
+      message: 'は5MB以下にしてください'
+    },
+    allow_nil: true
+
+  def display_image
+    if image.attached?
+      image.variant(resize_to_limit: [400, 300])
+    end
+  end
+
+  def small_image
+    if image.attached?
+      image.variant(resize_to_limit: [200, 150])
+    end
+  end
 end
