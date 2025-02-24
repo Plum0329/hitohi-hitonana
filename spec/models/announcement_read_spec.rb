a6e4e220e0ecba9e2f9dae05edfcfdcf9a82faca
Plum0329 <seiya19990329@gmail.com>
2025-02-25 01:59:04 +0900

    管理者からのお知らせ

--- spec/models/announcement_read_spec.rb
+++ spec/models/announcement_read_spec.rb
@@ -4,20 +4,11 @@ require 'rails_helper'
 
 RSpec.describe AnnouncementRead, type: :model do
   describe 'basic model' do
+    let(:user) { create(:user) }
+    let(:announcement) { create(:announcement) }
+
     it 'can be created with valid attributes' do
-      admin = create(:user, role: :admin)
-      user = create(:user)
-      announcement = Announcement.create!(
-        title: 'Test Title',
-        content: 'Test Content',
-        status: 'published',
-        priority: 'normal',
-        admin: admin
-      )
-      announcement_read = described_class.new(
-        user: user,
-        announcement: announcement
-      )
+      announcement_read = build(:announcement_read, user: user, announcement: announcement)
       expect(announcement_read).to be_valid
     end
   end
--- spec/models/announcement_spec.rb
+++ spec/models/announcement_spec.rb
@@ -4,15 +4,10 @@ require 'rails_helper'
 
 RSpec.describe Announcement, type: :model do
   describe 'basic model' do
+    let(:admin) { create(:user, role: :admin) }
+
     it 'can be created with valid attributes' do
-      admin = create(:user, role: :admin)
-      announcement = described_class.new(
-        title: 'Test Title',
-        content: 'Test Content',
-        status: 'published',
-        priority: 'normal',
-        admin: admin
-      )
+      announcement = build(:announcement, admin: admin)
       expect(announcement).to be_valid
     end
   end
